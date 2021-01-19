e3475152665763c8f2d15e9336da95c91ca5bd21
Aykut KIRCAN <aykutkircan@icloud.com>
2021-01-19 22:26:36 +0300

    feat: implement static files in hapijs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Hapi = require("@hapi/hapi");
 const Handlebars = require("handlebars");
 const Vision = require('@hapi/vision');
 const Path = require("path");
+const Inert = require("@hapi/inert");
 require("dotenv").config({path: __dirname + "/.env"});
 
 const PORT = process.env.PORT || 3000;
@@ -12,10 +13,35 @@ const init = async ()=>{
 
     const server = Hapi.server({
         host: "localhost",
-        port: PORT
+        port: PORT,
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'assets')
+            }
+        }
     });
+    
+    const plugin = {
+        name: "file",
+        multiple: false,
+        register: (server)=>{
+            server.route({
+                path: "/assets/{path*}",
+                method: "GET",
+                config: {
+                    auth: false,
+                    handler: {
+                        directory: {
+                            path: Path.join(__dirname, "assets"),
+                        }
+                    }
+                }
+            })
+        }
+    }
 
     await server.register(Vision);
+    await server.register(Inert);
 
     server.views({
         engines: {
@@ -26,10 +52,31 @@ const init = async ()=>{
 
     server.route({
         method: "GET",
+        path: "/assets/{path*}",
+        handler: {
+            directory: {
+                path: Path.join(__dirname, "assets"),
+                redirectToSlash: true,
+                index: true,
+            }
+        }
+    });
+
+    server.route({
+        method: "GET",
         path: "/",
         handler: (request, h)=>{
     
             return h.view("welcome.html");
+        }
+    });
+
+    server.route({
+        method: "GET",
+        path: "/kadro",
+        handler: (request, h)=>{
+    
+            return h.view("kadro.html");
         }
     });
 
